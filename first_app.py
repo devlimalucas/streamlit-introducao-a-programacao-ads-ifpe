0608d9fd032bba1eda0de4092512e0062a58cfd5
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-12-18 17:56:32 -0300

    feat: session state

--- first_app.py
+++ first_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.text_input("Your name", key="name")
 
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+# You can access the value at any point with:
+st.session_state.name
